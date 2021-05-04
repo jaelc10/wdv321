461ec11005164064a284075355b7004f9a67682d
jaelc10 <jael_0911@hotmail.com>
2021-05-03 22:32:07 -0500

    Javascript Final

---
+++ FinalJavascript/js/recipe.js
@@ -0,0 +1,121 @@
+
+// chili recipe
+let chiliIngredients = { chiliIngredientFull: "<p><ul><li>2 tbsp. cooking oil</li><li>1 cup onion</li><li>1 cup chopped peppers</li><li>4 tbsp. Chili powder</li><li>1 tsp. Hot chili powder (optional)</li><li>1 lb ground beef or chicken</li><li>2 cans Red Beans</li><li>2 cans Kidney Beans</li><li>2 cans Tomato Puree</li><li>2 cans Tomato Sauce</li><li>1 cup shredded cheese (optional)</li><li>1/2 cup sour cream (optional)</li></ul></p>", 
+                        chiliIngredientHalf: "<p><ul><li>1 tbsp. cooking oil</li><li>1/2 cup onion</li><li>1/2 cup chopped peppers</li><li>2 tbsp. Chili powder</li><li>1/2 tsp. Hot chili powder (optional)</li><li>1/2 lb ground beef or chicken</li><li>1 can Red Beans</li><li>1 can Kidney Beans</li><li>1 can Tomato Puree</li><li>1 can Tomato Sauce</li><li>1/2 cup shredded cheese (optional)</li><li>1/4 cup sour cream (optional)</li></ul></p>", 
+                        chiliIngredientDouble: "<p><ul><li>4 tbsp. cooking oil</li><li>2 cups onion</li><li>2 cups chopped peppers</li><li>8 tbsp. Chili powder</li><li>2 tsp. Hot chili powder (optional)</li><li>2 lbs ground beef or chicken</li><li>4 cans Red Beans</li><li>4 cans Kidney Beans</li><li>4 cans Tomato Puree</li><li>4 cans Tomato Sauce</li><li>2 cups shredded cheese (optional)</li><li>1 cup sour cream (optional)</li></ul></p>" };
+
+let chiliPrep = { chiliPrepFull: "<p><ol><li>Heat cooking oil in 2 quart skillet.</li><li>Saute onions and peppers for 5 minutes.</li><li>Add spices and stir for 30 seconds.</li><li>Add meat and cook until browned. Approximately 15 minutes.</li><li>Pour contents of skillet into 3 quart crock pot.</li><li>Rinse beans and place in crockpot.</li><li>Open and pour Tomato puree and sauce into crock pot.</li><li>Cover crockpot and cook on low for 6 hours.</li><li>Serve into individual bowls and top with sour cream and cheese.</li></ol></p>", 
+                  chiliPrepHalf: "<p><ol><li>Heat cooking oil in 1 quart skillet.</li><li>Saute onions and peppers for 5 minutes.</li><li>Add spices and stir for 30 seconds.</li><li>Add meat and cook until browned. Approximately 10 minutes.</li><li>Pour contents of skillet into 2 quart crock pot.</li><li>Rinse beans and place in crockpot.</li><li>Open and pour Tomato puree and sauce into crock pot.</li><li>Cover crockpot and cook on low for 4 hours.</li><li>Serve into individual bowls and top with sour cream and cheese.</li></ol></p>",
+                  chiliPrepDouble: "<p><ol><li>Heat cooking oil in 4 quart skillet.</li><li>Saute onions and peppers for 10 minutes.</li><li>Add spices and stir for 30 seconds.</li><li>Add meat and cook until browned. Approximately 25 minutes.</li><li>Pour contents of skillet into 6 quart crock pot.</li><li>Rinse beans and place in crockpot.</li><li>Open and pour Tomato puree and sauce into crock pot.</li><li>Cover crockpot and cook on low for 8 hours.</li><li>Serve into individual bowls and top with sour cream and cheese.</li></ol></p>" };
+
+let chiliInfo = { chiliInfoFull: "<p><strong>Serves:</strong>6</p><p><strong>Preparation:</strong>25 minutes</p><p><strong>Cooking:</strong>6 hours</p><p><strong>Difficulty:</strong>Easy</p>",
+                  chiliInfoHalf: "<p><strong>Serves:</strong>3</p><p><strong>Preparation:</strong>25 minutes</p><p><strong>Cooking:</strong>6 hours</p><p><strong>Difficulty:</strong>Easy</p>",
+                 chiliInfoDouble: "<p><strong>Serves:</strong>12</p><p><strong>Preparation:</strong>25 minutes</p><p><strong>Cooking:</strong>6 hours</p><p><strong>Difficulty:</strong>Easy</p>" };
+//end of chili recipe
+
+
+// start pizza rice
+let pizzaIngredients = { pizzaIngredientFull: "<p><ul><li>2 tbsp Extra-virgin olive oil divided</li><li>5 cloves garlic  quartered</li><li>3/4 tsp onion powder</li><li>1/2 tsp Freshly ground black pepper</li><li>1/2 Kosher salt</li><li>3/4 Tsp italian seasoning</li><li>3c Cooked rice</li><li>1/2c freshly Grated parmesan</li><li>1 (14-oz.) can crushed tomato<li>1 large clove garlic, grated</li><li>1/4 tsp. coarsely ground black pepper</li><li>1 tsp. kosher salt</li><li>1/4 tsp. crushed red pepper flakes</li><li>1 tbsp. granulated suga</li></ul></p>",
+                         pizzaIngredientHalf: "<p><ul><li>1 tbsp Extra-virgin olive oil divided</li><li>3 cloves garlic  quartered</li><li>1/4 tsp onion powder</li><li>1 tsp Freshly ground black pepper</li><li>1/2 Kosher salt</li><li>3/4 Tsp italian seasoning</li><li>3c Cooked rice</li><li>1/2c freshly Grated parmesan</li><li>1 (14-oz.) can crushed tomato<li>1 large clove garlic, grated</li><li>1/4 tsp. coarsely ground black pepper</li><li>1 tsp. kosher salt</li><li>1/4 tsp. crushed red pepper flakes</li><li>1 tbsp. granulated suga</li></ul></p>", 
+                         pizzaIngredientDouble: "<p><ul><li>4 tbsp Extra-virgin olive oil divided</li><li>10 cloves garlic  quartered</li><li>1c tsp onion powder</li><li>1 1/2 tsp Freshly ground black pepper</li><li>1/2 Kosher salt</li><li>3/4 Tsp italian seasoning</li><li>3c Cooked rice</li><li>1/2c freshly Grated parmesan</li><li>1 (14-oz.) can crushed tomato<li>1 large clove garlic, grated</li><li>1/4 tsp. coarsely ground black pepper</li><li>1 tsp. kosher salt</li><li>1/4 tsp. crushed red pepper flakes</li><li>1 tbsp. granulated suga</li></ul></p>" };
+
+let pizzaPrep = { pizzaPrepFull: "<p>1) Make the rice: In a 10” oven-safe skillet over medium heat, heat 1 tablespoon oil. Add garlic, onion, pepper, salt, red pepper flakes, and Italian seasoning, and cook until garlic turns golden, 2 minutes. Add tomato paste and cook until slightly caramelized, about 1 minute. Add nutmeg, remaining 1 tablespoon oil, and rice, and cook until rice begins to turn slightly crispy, about 8 minutes. Reduce heat to the lowest setting, then stir in ½ cup Parmesan and sesame until well combined.</p><p> 2) Preheat oven to 400°. Make the sauce: In a medium bowl, stir together all sauce ingredients until well combined. </p><p>Assemble the “pizza”: Spread ½ cup tomato sauce over fried rice in an even layer, then sprinkle mozzarella and Parmesan evenly across, and top with pepperoni.Transfer skillet to the oven and bake until cheese is melty and edges are golden</p><p>about 10 minutes.Garnish with basil before serving.</p>", 
+                  pizzaPrepHalf: "<p>1) Make the rice: In a 10” oven-safe skillet over medium heat, heat 1 tablespoon oil. Add garlic, onion, pepper, salt, red pepper flakes, and Italian seasoning, and cook until garlic turns golden, 2 minutes. Add tomato paste and cook until slightly caramelized, about 1 minute. Add nutmeg, remaining 1 tablespoon oil, and rice, and cook until rice begins to turn slightly crispy, about 8 minutes. Reduce heat to the lowest setting, then stir in ½ cup Parmesan and sesame until well combined.</p><p> 2) Preheat oven to 400°. Make the sauce: In a medium bowl, stir together all sauce ingredients until well combined. </p><p>Assemble the “pizza”: Spread ½ cup tomato sauce over fried rice in an even layer, then sprinkle mozzarella and Parmesan evenly across, and top with pepperoni.Transfer skillet to the oven and bake until cheese is melty and edges are golden</p><p>about 10 minutes.Garnish with basil before serving.</p>", 
+                  pizzaPrepDouble: "<p>1) Make the rice: In a 10” oven-safe skillet over medium heat, heat 1 tablespoon oil. Add garlic, onion, pepper, salt, red pepper flakes, and Italian seasoning, and cook until garlic turns golden, 2 minutes. Add tomato paste and cook until slightly caramelized, about 1 minute. Add nutmeg, remaining 1 tablespoon oil, and rice, and cook until rice begins to turn slightly crispy, about 8 minutes. Reduce heat to the lowest setting, then stir in ½ cup Parmesan and sesame until well combined.</p><p> 2) Preheat oven to 400°. Make the sauce: In a medium bowl, stir together all sauce ingredients until well combined. </p><p>Assemble the “pizza”: Spread ½ cup tomato sauce over fried rice in an even layer, then sprinkle mozzarella and Parmesan evenly across, and top with pepperoni.Transfer skillet to the oven and bake until cheese is melty and edges are golden</p><p>about 10 minutes.Garnish with basil before serving.</p>"};
+
+
+let pizzaInfo = { pizzaInfoFull: "<p><strong>Serves:</strong>6</p><p><strong>Preparation:</strong>2 Hours</p><p><strong>Cooking:</strong>8 minutes</p><p><strong>Difficulty:</strong>Easy</p>", 
+                  pizzaInfoHalf: "<p><strong>Serves:</strong>3</p><p><strong>Preparation:</strong>2 Hours</p><p><strong>Cooking:</strong>8 minutes</p><p><strong>Difficulty:</strong>Easy</p>",
+                  pizzaInfoDouble: "<p><strong>Serves:</strong>12</p><p><strong>Preparation:</strong>2 Hours</p><p><strong>Cooking:</strong>8 minutes</p><p><strong>Difficulty:</strong>Easy</p>" };
+// end of pizza rice
+
+// start lemon chicken
+let lemonChickenIngredients = { lemonChickenIngredientFull: "<p><ul><li>1/4 c. low-sodium soy sauce</li><li>1/4 c. extra-virgin olive oil</li><li>6 cloves garlic, minced</li><li>Juice of 1 lemon</li><li>Kosher salt</li><li>Freshly ground black pepper</li><li>Pinch crushed red pepper flakes</li><li>8 chicken drumsticks (about 3 lbs.)</li></ul></p>",
+                           lemonChickenIngredientHalf: "<p><ul><li>1/8 c. low-sodium soy sauce</li><li>1/8 c. extra-virgin olive oil</li><li>3 cloves garlic, minced</li><li>Juice of 1/2 lemon</li><li>Kosher salt</li><li>Freshly ground black pepper</li><li>Pinch crushed red pepper flakes</li><li>4 chicken drumsticks (about 2 lbs.)</li></ul></p>", 
+                           lemonChickenIngredientDouble: "<p><ul><li>1/2 c. low-sodium soy sauce</li><li>1/2 c. extra-virgin olive oil</li><li>12 cloves garlic, minced</li><li>Juice of 2 lemon</li><li>Kosher salt</li><li>Freshly ground black pepper</li><li>Pinch crushed red pepper flakes</li><li>16 chicken drumsticks (about 6 lbs.)</li></ul></p>" };
+
+let lemonChickenPrep = { lemonChickenPrepFull: "<ol><li>In a large bowl, combine soy sauce, oil, garlic, and lemon juice. Season with salt, pepper, and a pinch of red pepper flakes. Add drumsticks and toss to coat. Refrigerate and let marinate at least 30 minutes and up to overnight.</li><li>2) Preheat oven to 450°. Place chicken on a large baking sheet and bake until golden and internal temperature reaches 165°, 20 to 25 minutes, flipping halfway through. </li></ol>",
+                   lemonChickenPrepHalf: "<ol><li>In a large bowl, combine soy sauce, oil, garlic, and lemon juice. Season with salt, pepper, and a pinch of red pepper flakes. Add drumsticks and toss to coat. Refrigerate and let marinate at least 30 minutes and up to overnight.</li><li>2) Preheat oven to 450°. Place chicken on a large baking sheet and bake until golden and internal temperature reaches 165°, 20 to 25 minutes, flipping halfway through. </li></ol>", 
+                    lemonChickenPrepDouble: "<ol><li>In a large bowl, combine soy sauce, oil, garlic, and lemon juice. Season with salt, pepper, and a pinch of red pepper flakes. Add drumsticks and toss to coat. Refrigerate and let marinate at least 30 minutes and up to overnight.</li><li>2) Preheat oven to 450°. Place chicken on a large baking sheet and bake until golden and internal temperature reaches 165°, 20 to 25 minutes, flipping halfway through. </li></ol>"};
+
+let lemonChickenInfo = { lemonChickenInfoFull: "<p><strong>Serves: 4</strong></p><p><strong>Preparation: 110 minutes</strong></p><p><strong>Cooking: 2 hours</strong></p><p><strong>Difficulty: Hard</strong></p>",
+                    lemonChickenInfoHalf: "<p><strong>Serves: 2</strong></p><p><strong>Preparation: 35 minutes</strong></p><p><strong>Cooking: 2 hours</strong></p><p><strong>Difficulty: Hard</strong></p>",
+                    lemonChickenInfoDouble: "<p><strong>Serves: 8</strong></p><p><strong>Preparation: 140 minutes</strong></p><p><strong>Cooking: 2 hours</strong></p><p><strong>Difficulty: Hard</strong></p>" };
+
+// end lemon recipe
+
+
+
+// function for recipes
+function chiliRecipe() {
+    if (document.getElementById("full").checked) {
+        document.getElementById("chili-ingredients").innerHTML = chiliIngredients.chiliIngredientFull;
+        document.getElementById("chili-prep").innerHTML = chiliPrep.chiliPrepFull;
+        document.getElementById("chili-info").innerHTML = chiliInfo.chiliInfoFull;
+
+
+    } else if (document.getElementById("half").checked) {
+        document.getElementById("chili-ingredients").innerHTML = chiliIngredients.chiliIngredientHalf;
+        document.getElementById("chili-prep").innerHTML = chiliPrep.chiliPrepHalf;
+        document.getElementById("chili-info").innerHTML = chiliInfo.chiliInfoHalf;
+
+
+    } else if (document.getElementById("double").checked) {
+        document.getElementById("chili-ingredients").innerHTML = chiliIngredients.chiliIngredientDouble;
+        document.getElementById("chili-prep").innerHTML = chiliPrep.chiliPrepHalf;
+        document.getElementById("chili-info").innerHTML = chiliInfo.chiliInfoDouble;
+
+
+
+    }
+}
+
+function pizzaRice() {
+    if (document.getElementById("pizza-full").checked) {
+        document.getElementById("pizza-ingredients").innerHTML = pizzaIngredients.pizzaIngredientFull;
+        document.getElementById("pizza-prep").innerHTML = pizzaPrep.pizzaPrepFull;
+        document.getElementById("pizza-info").innerHTML = pizzaInfo.pizzaInfoFull;
+    } else if (document.getElementById("pizza-half").checked) {
+        document.getElementById("pizza-ingredients").innerHTML = pizzaIngredients.pizzaIngredientHalf;
+        document.getElementById("pizza-prep").innerHTML = pizzaPrep.pizzaPrepHalf;
+        document.getElementById("pizza-info").innerHTML = pizzaInfo.pizzaInfoHalf;
+    } else if (document.getElementById("pizza-double").checked) {
+        document.getElementById("pizza-ingredients").innerHTML = pizzaIngredients.pizzaIngredientDouble;
+        document.getElementById("pizza-prep").innerHTML = pizzaPrep.pizzaPrepDouble;
+        document.getElementById("pizza-info").innerHTML = pizzaInfo.pizzaInfoDouble;
+    }
+}
+
+function lemonChicken() {
+    if (document.getElementById("lemonChicken-full").checked) {
+        document.getElementById("lemonChicken-ingredients").innerHTML = lemonChickenIngredients.lemonChickenIngredientFull;
+        document.getElementById("lemonChicken-prep").innerHTML = lemonChickenPrep.lemonChickenPrepFull;
+        document.getElementById("lemonChicken-info").innerHTML = lemonChickenInfo.lemonChickenInfoFull;
+    } else if (document.getElementById("lemonChicken-half").checked) {
+        document.getElementById("lemonChicken-ingredients").innerHTML = lemonChickenIngredients.lemonChickenIngredientHalf;
+        document.getElementById("lemonChicken-prep").innerHTML = lemonChickenPrep.lemonChickenPrepHalf;
+        document.getElementById("lemonChicken-info").innerHTML = lemonChickenInfo.lemonChickenInfoHalf;
+    } else if (document.getElementById("lemonChicken-double").checked) {
+        document.getElementById("lemonChicken-ingredients").innerHTML = lemonChickenIngredients.lemonChickenIngredientDouble;
+        document.getElementById("lemonChicken-prep").innerHTML = lemonChickenPrep.lemonChickenPrepDouble;
+        document.getElementById("lemonChicken-info").innerHTML = lemonChickenInfo.lemonChickenInfoDouble;
+    }
+}
+// end of recipe functions
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+});
